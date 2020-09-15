d5c702d5e264e64a85157408d3e729fb8d9dc523
Valentin <valentin.rublack@gmail.com>
2020-09-15 17:23:22 +0200

    Fixed docstring

--- fairseq_cli/extract_sequence_embeddings.py
+++ fairseq_cli/extract_sequence_embeddings.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 """
-Translate raw text with a trained model. Batches data on-the-fly.
+Extracts sequence embeddings from trained model. Batches data on-the-fly.
 """
 
 from collections import namedtuple
